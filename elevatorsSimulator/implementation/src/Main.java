2670eba67fb7141345712b9fa3dded52c3e1b483
foioguto <augusto.alves0510@gmail.com>
2025-05-17 16:22:56 -0300

    att

--- elevatorsSimulator/implementation/src/Main.java
+++ elevatorsSimulator/implementation/src/Main.java
@@ -4,9 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         EventsList eventsList = new EventsList();
-        
-        eventsList.setEvent("startBuildingManual");
-        eventsList.callEvent();
 
         eventsList.setEvent("startElevator");
         eventsList.callEvent();
